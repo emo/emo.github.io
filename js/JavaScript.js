064062682b3c22536a90bd8fbcac9fc1504ac5ae
lilyal <1lilyal1@mail.ru>
2020-08-12 23:54:17 +0500

    вот и сайт

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,18 +1,32 @@
-﻿function loop(div) {
-    async.forever(
-        function (next) {
-            div.animate({ height: '500px', opacity: '0.4' }, "slow");
-            div.animate({ width: '500px', opacity: '0.8' }, "slow");
-            div.animate({ height: '100px', opacity: '0.4' }, "slow");
-            div.animate({ width: '100px', opacity: '0.8' }, "slow");
-            next();
-        },
-        function (err) {
-            console.error(err);
+﻿document.getElementById("defaultOpen").click();
+
+
+
+var Division1 = [
+    [1, "Савельев Владислав Агафонович", "enalia", "Ena1337"],
+    [2, "Сергеева Маша Эдуардовичина", "Маша | Qeenon", "Маша"]
+];
+
+var Division2 = [
+    [1, "Савельев Владислав Агафонович", "Петруша", "PETRUSHKA"],
+    [2, "Лилуальный", "Лилуал", "Лилуал#2400"],
+];
+
+LoadTable(1);
+LoadTable(2);
+
+function LoadTable(Table) {
+	if (Table == 1) {
+        window.tableDivision1.innerHTML = "";
+        window.tableDivision1.innerHTML += "<tr><td>FullName</td></td><td>Discord</td><td>Battletag</td></tr>";
+        for (let i = 0; i < window.Division1.length; i++) {
+            window.tableDivision1.innerHTML += "<tr class='" + window.Division1[i][0] + "'><td>" + window.Division1[i][1] + "</td><td>" + window.Division1[i][2] + "</td><td>" + window.Division1[i][3] + "</td></tr>";
+		}
+	} else {
+        window.tableDivision2.innerHTML = "";
+        window.tableDivision2.innerHTML += "<tr><td>FullName</td></td><td>Discord</td><td>Battletag</td></tr>";
+        for (let i = 0; i < window.Division2.length; i++) {
+            window.tableDivision2.innerHTML += "<tr class='" + window.Division2[i][0] + "'><td>" + window.Division2[i][1] + "</td><td>" + window.Division2[i][2] + "</td><td>" + window.Division2[i][3] + "</td></tr>";
         }
-    );
+	}
 }
-$(document).ready(function () {
-    var d = $("div");
-    loop(d);
-});
