2d03a85174624d69ddc210e08e223885d010777e
lilyal <1lilyal1@mail.ru>
2020-08-12 19:22:52 +0500

    Тест

---
+++ js/JavaScript.js
@@ -0,0 +1,18 @@
+﻿function loop(div) {
+    async.forever(
+        function (next) {
+            div.animate({ height: '500px', opacity: '0.4' }, "slow");
+            div.animate({ width: '500px', opacity: '0.8' }, "slow");
+            div.animate({ height: '100px', opacity: '0.4' }, "slow");
+            div.animate({ width: '100px', opacity: '0.8' }, "slow");
+            next();
+        },
+        function (err) {
+            console.error(err);
+        }
+    );
+}
+$(document).ready(function () {
+    var d = $("div");
+    loop(d);
+});
